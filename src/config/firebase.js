593b20116c0591950972ca3b1daadb25162f6b7f
debasish_sahoo@monosage.com <debasishsahoolipu12@gmail.com>
2025-04-15 13:00:20 +0530

    15-04-2025

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,26 +1,15 @@
-// firebase.js
 import { initializeApp, cert } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
-import { readFile } from "fs/promises";
-import path from "path";
-import { fileURLToPath } from "url";
 
-// Setup __dirname in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Load service account JSON
+// Parse service account JSON from the environment variable
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-const serviceAccountPath = path.join(__dirname, serviceAccount);
-const serviceAccountJSON = JSON.parse(await readFile(serviceAccountPath, "utf-8"));
 
 // Initialize Firebase
 initializeApp({
-  credential: cert(serviceAccountJSON),
+  credential: cert(serviceAccount),
   storageBucket: "book-app-3eb7b.appspot.com",
 });
 
-// Export bucket
+// Export bucket for use in your code
 const bucket = getStorage().bucket();
-
 export { bucket };
